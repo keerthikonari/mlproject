7f1993d68ff2e48600b2aa24630255eb88e2cd8a
keerthikonari <keerthikonari430@gmail.com>
2026-07-19 15:58:19 +0400

    Hyper Parameter Training

--- src/componenets/model_trainer.py
+++ src/componenets/model_trainer.py
@@ -45,10 +45,49 @@ class ModelTrainer:
                 "XGBoost": XGBRegressor(),
             
             }
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error','absolute_error','poisson'],
+                    'max_features':['sqrt','log2'],
+                    'n_estimators':[8,16,32,64,128,256]
+                    
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error','huber','absolute_error','quantile'],
+                    #'learning_rate':[-1,01,05,001],
+                    'criterion':['squared_error','friedman_mse'],
+                    'max_features':['auto','sqrt','log2'],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Nearest Neighbors":{
+                    "n_neighbors":[5,7,9,11],
+                    'weights':['uniform','distance'],
+                    'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBoost":{
+                   # 'learning_rate':[-1,01,05,001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost":{
+                    'depth':[6,8,10],
+                   # 'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost":{
+                   # 'learning_rate':[-1,01,05,001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+            }
             
             
-            
-            model_report:dict=evaluate_models(x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test,models=models)
+            model_report:dict=evaluate_models(x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test,models=models,params=params)
             # to get best model score from cist
             best_model_score=max(model_report.values())
             
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -16,11 +17,16 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(x_train,x_test,y_train,y_test,models):
+def evaluate_models(x_train,x_test,y_train,y_test,models,params):
     try:
         report={}
         for i in range(len(models)): 
             model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
+           
             
             model.fit(x_train,y_train)
             
